eab8103c22766636849b777c1b2579cbca0f7f4e
EKTC <ethancao@y7mail.com>
2025-01-16 13:28:49 +1100

    Adding to climb stairs

--- Dynamic_Programming/climb_stairs.py
+++ Dynamic_Programming/climb_stairs.py
@@ -1,3 +1,6 @@
+# The whole point is to reuse values that we have already precalculated so we do not have to recurse any further
+# So we use cached values and add onto it as we go
+# We return memo[n] as thats the number of ways which we want to find
 def climbStairs(n):
     # How this works is that you can only go 1 or 2
     # So you have to recursively call the function
@@ -16,6 +19,17 @@ def climbStairs(n):
     memo = {1: 1, 2: 2}  # base cases
     return climb(n)
     
+# The more naive approach that takes a lot longer
+# Simple base cases with the recursive step
+def climbStairsV2(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
         
+    return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
 if __name__ == "__main__":
     print(climbStairs(5))
